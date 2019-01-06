51fd0b761f659a52df0169a13b2457f210129987
dickanirwansyah <dicka.nirwansyah@fusi24.com>
2019-01-07 06:58:58 +0700

    add morgan log dan nodemon dev

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,8 +10,43 @@ router.get('/', (req, res, next) => {
 
 /** post */
 router.post('/', (req, res, next) => {
-    res.status(200).json({
+    res.status(201).json({
         message: 'POST Handling router products.'
+    });
+});
+
+/** get by id */
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'id is special.',
+            id: id
+        })
+    }else if(id === 'notspecial'){
+        res.status(200).json({
+            message: 'id is not special',
+            id: id
+        })
+    }else{
+        res.status(200).json({
+            message: 'id is not special or notspecial'
+        })
+    }
+});
+
+/**patch */
+router.patch('/:productId', (req, res, next) => {
+   res.status(200).json({
+        message: 'Updated Products.'
+    });
+});
+
+/** delete */
+router.delete('/:productId', (req, res, nexr) => {
+    res.status(200).json({
+        message: 'Delete Products.'
     });
 });
 
--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-const productRoutes = require('./api/routes/products')
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/order');
 
+//morgan log
+app.use(morgan('dev'));
+
+//router handling
 app.use('/products', productRoutes);
+app.use('/order', orderRoutes);
+
+
+//router handling error
+app.use((req, res, next) => {
+    const error = new Error('Router Notfound..');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+          message: error.message
+        }
+    });
+});
 
 module.exports = app;
